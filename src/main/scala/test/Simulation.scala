d85d9ad0ef5c1f1b92fd59a39e55be35a4de5d9c
Peter Morton Sr <darthpemo@hotmail.com>
2020-01-25 09:08:37 -0500

    Add event output from step

--- src/main/scala/test/Simulation.scala
+++ src/main/scala/test/Simulation.scala
@@ -10,12 +10,12 @@ sealed class GameEvent
 
 case class PlayerJoined(
 	name: String
-)
+) extends GameEvent
 
 case class CreepSpawned(
 	name    : String,
 	location: (Int, Int)
-)
+) extends GameEvent
 
 sealed class GameCommand
 
@@ -39,22 +39,22 @@ object Simulator {
 		gameCommand match {
 			case a: JoinGame =>
 				println(s"Player ${a.name} has joined the game")
-				(gameState.copy(players = a.name :: gameState.players), events)
+				(gameState.copy(players = a.name :: gameState.players), PlayerJoined(a.name) ::  events)
 			case b: PlaceCreep =>
 				println(s"Creep spotted - ${b.name}")
-				(gameState.copy(creeps = Creep(b.name, (0, 10)) :: gameState.creeps), events)
+				(gameState.copy(creeps = Creep(b.name, (0, 10)) :: gameState.creeps), CreepSpawned(b.name, (0,10)) :: events)
 		}
 	}
 
 	@scala.annotation.tailrec
 	def processCommands(gameCommands: NonEmptyList[GameCommand], gameState: GameState, events: List[GameEvent]): (GameState, List[GameEvent]) = {
 		val gameCommand = gameCommands.head
-		val (newGameState, events) = processCommand(gameCommand, gameState, List())
+		val (newGameState, newEvents) = processCommand(gameCommand, gameState, events)
 
 		if (gameCommands.tail.isEmpty) {
-			(newGameState, events)
+			(newGameState, newEvents)
 		} else {
-			processCommands(NonEmptyList(gameCommands.tail.head, gameCommands.tail.tail), newGameState, events)
+			processCommands(NonEmptyList(gameCommands.tail.head, gameCommands.tail.tail), newGameState, newEvents)
 		}
 	}
 
@@ -71,7 +71,11 @@ object Simulator {
 		val gameState = GameState()
 
 		val commands = JoinGame("Bobby") :: JoinGame("Someone") :: Nil
-		println(step(commands, gameState))
+		val (state1, events1) = step(commands, gameState)
+		println(s"Step 1 events: $events1")
+
+		val (state2, events2) = step(PlaceCreep("Robot creep") :: Nil, state1)
+		println(s"Step 2 events: $events2")
 	}
 
 }
